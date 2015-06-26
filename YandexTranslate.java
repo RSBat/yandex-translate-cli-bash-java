84c693a53f119ba3344fbfbc076cbc4dd0b46921
Vitaly Zdanevich <zdanevich.vitaly@ya.ru>
2015-06-26 22:51:33 +0300

    init

--- YandexTranslate.java
+++ YandexTranslate.java
@@ -8,8 +8,8 @@ public class YandexTranslate {
 		System.out.println(translate("ru", args[0]));
 	}
 
-	private static String translate(String lang, String text) throws IOException {
-		String textEscaped = text.replace(" ", "%20");
+	private static String translate(String lang, String enteredText) throws IOException {
+		String textEscaped = enteredText.replace(" ", "%20");
 		String url = "https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20150402T173446Z.82a90fe78ca2aeaf.a3bd7c7a0f72b260e28f5d92e4f242cf6ba189d3&lang="
 				+ lang + "&text=" + textEscaped;
 		URLConnection connection = null;
@@ -23,8 +23,8 @@ public class YandexTranslate {
 		int start = json.indexOf("[");
 		int end = json.indexOf("]");
 		String translated = json.substring(start + 2, end - 1);
-		if (translated.equals(text)) {
-			return translate("en", text);
+		if (translated.equals(enteredText)) {
+			return translate("en", enteredText);
 		} else return translated;
 	}
 }
