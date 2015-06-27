cbfe1beeea539202596c3decb72796f5ea042056
Vitaly Zdanevich <zdanevich.vitaly@ya.ru>
2015-06-27 10:41:46 +0300

    After feedback of Habr: now user can insert text with symbol '&', for example trans 'r&d'; recursion is not infinite

--- YandexTranslate.java
+++ YandexTranslate.java
@@ -1,31 +1,34 @@
+import javax.net.ssl.HttpsURLConnection;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
-import java.net.URLConnection;
+import java.net.URLEncoder;
 
 public class YandexTranslate {
+	private static int i = 0;
 	public static void main(String[] args) throws IOException {
 		System.out.println(translate("ru", args[0]));
 	}
 
-	private static String translate(String lang, String enteredText) throws IOException {
-		String textEscaped = enteredText.replace(" ", "%20");
-		String url = "https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20150402T173446Z.82a90fe78ca2aeaf.a3bd7c7a0f72b260e28f5d92e4f242cf6ba189d3&lang="
-				+ lang + "&text=" + textEscaped;
-		URLConnection connection = null;
-		try {
-			connection = new URL(url).openConnection();
-		} catch (ArrayIndexOutOfBoundsException e) {
-			System.out.printf("No text for translate");
-		}
+	private static String translate(String lang, String input) throws IOException {
+		String urlStr = "https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20150627T071448Z.117dacaac1e63b79.6b1b4bb84635161fcd400dace9fb2220d6f344ef";
+		URL urlObj = new URL(urlStr);
+		HttpsURLConnection connection = (HttpsURLConnection)urlObj.openConnection();
+		connection.setRequestMethod("POST");
+		connection.setDoOutput(true);
+		DataOutputStream dataOutputStream = new DataOutputStream(connection.getOutputStream());
+		dataOutputStream.writeBytes("text=" + URLEncoder.encode(input, "UTF-8") + "&lang=" + lang);
+
 		InputStream response = connection.getInputStream();
 		String json = new java.util.Scanner(response).nextLine();
 		int start = json.indexOf("[");
 		int end = json.indexOf("]");
 		String translated = json.substring(start + 2, end - 1);
-		if (translated.equals(enteredText)) {
+		i++;
+		if (translated.equals(input) && i < 2) {
 			// if return equal of entered text - we need change direction of translation
-			return translate("en", enteredText);
+			return translate("en", input);
 		} else return translated;
 	}
 }
